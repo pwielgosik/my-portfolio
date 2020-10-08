a64c56e1b948f55972c9fa09692daf31cd83bca1
S50B32 <pwielgosik@gmail.com>
2020-10-08 19:27:10 +0200

    Update SEO.js
    preload fix

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -85,7 +85,15 @@ function SEO({ description, lang, meta, title }) {
         type="font/woff2"
         crossOrigin="anonymous"
       /> */}
-      <link rel="preload" as="stylesheet" href="/fonts/fonts.css" media="all" />
+      <link
+        rel="preload"
+        href="/fonts/fonts.css"
+        as="style"
+        onload="this.rel='stylesheet'"
+      />
+
+      {/* <link rel="preload" as="stylesheet" href="/fonts/fonts.css" media="all" />
+      <link rel="stylesheet" href="/fonts/fonts.css" media="all" /> */}
     </Helmet>
   )
 }
