dfc7c9313a723cbe0fb00dd84adae784c67574d3
S50B32 <pwielgosik@gmail.com>
2020-10-28 03:37:55 +0100

    Update SEO.js

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -9,7 +9,6 @@ import React from "react"
 import PropTypes from "prop-types"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
-import fontUrl from "../../static/fonts/futura-t-ot-medium.woff2"
 
 function SEO({ description, lang, meta, title }) {
   const { site } = useStaticQuery(
